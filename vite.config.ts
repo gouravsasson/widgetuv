0a52f265f3c59aafa72e1bb8061bcd74e4e3d0da
gouravsasson <sasson.gourav93@gmail.com>
2025-01-31 20:02:20 +0530

    chnage in

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/widget.tsx'), 
+      name: 'ReactWidget',
+      fileName: 'react-widget',
+      formats: ['iife'], 
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
