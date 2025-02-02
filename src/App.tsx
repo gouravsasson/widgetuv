b1fbbe186a1685c26af0b39d1ee954d77e26ec14
gouravsasson <sasson.gourav93@gmail.com>
2025-02-02 08:27:51 +0530

    remove bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { VoiceAssistant } from "./components/VoiceAssistant";
 function App() {
   return (
     <>
-      <div className=" bg-black flex items-center justify-center p-4 overflow-hidden">
+      <div className="flex items-center justify-center p-4 overflow-hidden">
         <VoiceAssistant
           onMessageSend={(message) => console.log("Message:", message)}
           onVoiceStart={() => console.log("Voice started")}
